99f70a493e84ae78a2e59cd5023ee54b98b02375
wxbt <Grace@mybitpla.net>
2021-05-24 13:18:06 +0800

    logs

---
+++ main.ts
@@ -0,0 +1,31 @@
+/**
+ * 红外遥控器makecode中文模块
+ */
+
+//% color="#606060" weight=25  icon="\uf143" block="红外遥控"
+
+namespace IR {
+
+  /**
+  * initialization
+  */
+  //% blockId=ir_init
+  //% blockGap=20 weight=90
+  //% block="将红外接收器连接到 %pin"
+  //% shim=IR::init
+  export function init(pin: Pins): void {
+    return
+  }
+
+  /**
+  * button pushed.
+  */
+  //% blockId=ir_received_event
+  //% blockGap=20 weight=70
+  //% block="当 |%btn| 键被按下"
+  //% shim=IR::onPressEvent
+  export function onPressEvent(btn: RemoteButton, body:Action): void {
+    return
+  }
+
+}
---
+++ shims.d.ts
@@ -0,0 +1,9 @@
+
+// Auto-generated. Do not edit.
+
+
+declare namespace IR {
+
+}
+
+// Auto-generated. Do not edit. Really.
